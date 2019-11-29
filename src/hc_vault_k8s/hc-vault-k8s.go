94f03ce9cc74f25f2843817a7f6b5a5050c881ff
oleggorj <oleggorj@hotmail.com>
2019-11-29 11:53:08 -0500

    ongoing development commits

--- src/hc_vault_k8s/hc-vault-k8s.go
+++ src/hc_vault_k8s/hc-vault-k8s.go
@@ -24,3 +24,37 @@ const (
 	AuthMountPath           = "auth/kubernetes"
 	ServiceAccountTokenPath = "/var/run/secrets/kubernetes.io/serviceaccount/token"
 )
+
+// VaultLogicalWriter interface for testing
+type vaultLogicalWriter interface {
+	Write(path string, data map[string]interface{}) (*api.Secret, error)
+}
+
+// vaultLogical will be overwritten by tests
+var vaultLogical = func(c *api.Client) vaultLogicalWriter {
+	return c.Logical()
+}
+
+// Vault represents the configuration to get a valid Vault token
+type Vault struct {
+	Role                    string
+	TokenPath               string
+	ReAuth                  bool
+	TTL                     int
+	AuthMountPath           string
+	ServiceAccountTokenPath string
+	AllowFail               bool
+	client                  *api.Client
+}
+
+// FixAuthMountPath add the auth prefix
+// kubernetes      -> auth/kubernetes
+// auth/kubernetes -> auth/kubernetes
+// presumes a valid path
+func FixAuthMountPath(p string) string {
+	pp := strings.Split(strings.TrimLeft(p, "/"), "/")
+	if pp[0] == "auth" {
+		return path.Join(pp...) // already correct
+	}
+	return path.Join(append([]string{"auth"}, pp...)...)
+}
