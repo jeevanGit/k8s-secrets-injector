566909c50cd17b3ead0122930ba62cbeabcf9209
oleggorj <oleggorj@hotmail.com>
2019-11-29 14:13:54 -0500

    ongoing development commits

--- src/hc_vault_kv/hc_vault_kv.go
+++ src/hc_vault_kv/hc_vault_kv.go
@@ -23,3 +23,15 @@ type Client struct {
 	client  *api.Client
 	Version int
 }
+
+// FixPath inserts the API prefix for v1 style path
+// secret/foo      -> secret/data/foo
+// secret/data/foo -> secret/data/foo
+// presumes a valid path
+func FixPath(p, prefix string) string {
+	pp := strings.Split(p, "/")
+	if pp[1] == prefix {
+		return p // already v2 style path
+	}
+	return path.Join(append(pp[:1], append([]string{prefix}, pp[1:]...)...)...)
+}
