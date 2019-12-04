71e14c1be8f6241b1cc769f685121557c4581450
oleggorj <oleggorj@hotmail.com>
2019-12-04 16:59:46 -0500

    ongoing development commits

--- src/secretsinjector/secrets-injector-hc-vault.go
+++ src/secretsinjector/secrets-injector-hc-vault.go
@@ -21,6 +21,8 @@ const (
 	hcVaultVarName         = "hashicorpvault"
 )
 
+
+
 // map of File Var and Secret path
 type FileVarStruct struct {
   Secrets map[string]string
@@ -34,8 +36,9 @@ type HCVaultSecretsInjectorStruct struct {
   Vault               *hcvault.HCVault
   VaultClients        map[string]*kv.VaultClient
   VaultToken          string
-	EnvVars 		        EnvVarStruct // Map of secrets pairs
-  FileVars            map[string]FileVarStruct  // map of File name to Map of secrets pairs
+	EnvVars 		        EnvVarStruct // Map of secrets pairs for environment variables
+  // One file may have multiple secrets
+  FileVars            map[string]FileVarStruct  // map of File name to Map of secret pairs (secret name - actual secret)
 }
 
 // Create new HC vault client and populate the environment in it
@@ -44,13 +47,11 @@ func NewHashicorpVault() (*HCVaultSecretsInjectorStruct, error) {
   v := &HCVaultSecretsInjectorStruct{}
   v.Vault, err = hcvault.NewFromEnvironment()
 	if err != nil {
-    s := fmt.Sprintf("error: %s ", err.Error() )
-    return v, errors.New(s)
+    return v, errors.New( fmt.Sprintf("error: %s ", err.Error() ) )
 	}
 	v.VaultToken, err = v.Vault.Authenticate()
 	if err != nil {
-      s := fmt.Sprintf("error: %s ", err.Error() )
-			return v, errors.New(s)
+			return v, errors.New( fmt.Sprintf("error: %s ", err.Error() ) )
 	}
   log.Debugf("successfully authenticated to vault")
 	log.Debugf("new token: %s", v.VaultToken)
@@ -81,58 +82,61 @@ func NewHashicorpVault() (*HCVaultSecretsInjectorStruct, error) {
   //  v.FileVars should have:  map[ "/etc/secrets":{ "secret_mysql" : "secret/${APP_NAME}/${NAMESPACE}/mysql" } ]
   //
   v.FileVars = make(map[string]FileVarStruct)
-  for _, pair := range os.Environ() {
-    kv := strings.SplitN( pair , "=" , 2 )
-    // check if var ends with "@hashicorpvault"
-    if strings.HasPrefix( strings.ToLower(kv[0]), patternSecretName ) {             // SECRET_INJECTOR_SECRET_NAME_secret_mysql
-      secVar := strings.TrimPrefix( strings.ToLower(kv[0]), patternSecretName )     // secret_mysql
-        fv := FileVarStruct{                                                     // temp FileVarStruct with "secret_mysql" and secret
+  for _, pair := range os.Environ() { // go through env vars one by one
+    kv := strings.SplitN( pair , "=" , 2 )  // and try to identify the patters
+    if strings.HasPrefix( strings.ToLower(kv[0]), patternSecretName ) {             // see if var name matches SECRET_INJECTOR_SECRET_NAME_<something>
+      secVar := strings.TrimPrefix( strings.ToLower(kv[0]), patternSecretName )     // <something>
+        fv := FileVarStruct{                                                     // temp FileVarStruct with <something> and secret
           Secrets: map[string]string{
             secVar: strings.ToLower(kv[1]),
           },
         }
 
         // look up corresponding Store System env var - it determines teh vault origin for the secret
-        storeSystem := utils.GetEnvVariableByName( patternStoreSystem + secVar )
-        if storeSystem == "" {   // finding SECRET_INJECTOR_MOUNT_PATH_secret_mysql
-          s := fmt.Sprintf("Missing Store System env variable for secret %s: '%s' - can not determine Vault origin", secVar, patternStoreSystem + secVar )
+        storeSystem := utils.GetEnvVariableByName( patternStoreSystem + secVar ) // see if var name matches SECRET_STORE_SYSTEM_<something>
+        if storeSystem == "" {
+
+          s := fmt.Sprintf("Missing Store System env variable for secret %s: '%s' - can not determine Vault origin. Skipping secret ''.", secVar, patternStoreSystem + secVar, secVar )
           return v, errors.New(s)
-        }else{
+
+        }else{  // aha! baby is comming from HC!
 
           // if match then secret comes from HC Vault
           if storeSystem == hcVaultVarName {
             // look up second corresponding env var with name "secret_injector_mount_path_" + secVar,
             //   to determine mount path
             mountPath := utils.GetEnvVariableByName( patternSecretMountPath + secVar )
-            if mountPath == "" {   // finding SECRET_INJECTOR_MOUNT_PATH_
+            if mountPath == "" {   // finding SECRET_INJECTOR_MOUNT_PATH_<something>
               s := fmt.Sprintf("Missing second set of env variables for secret %s: '%s'", secVar, patternSecretMountPath + secVar )
               return v, errors.New(s)
             }
-            var newKey string
+            newKey := strings.TrimSuffix((mountPath, "/")) + "/" + secVar
+/*
             if strings.HasSuffix(mountPath, "/") {
               newKey = mountPath + secVar
             }else{
               newKey = mountPath + "/" + secVar
-            }
+            } */
             v.FileVars[newKey] = fv       // create new map entry "file path + secret name": {"secret name", "secret path in vault"}
-            log.Debugf("Adding new entry for File Variables map: %s", newKey)
+            log.Debugf("Adding new entry for File Variables map: %s", newKey)  // we've got new crew-mate!
 
           } // else secret from Az KVault
         }
 
-    }
+    } // nope.. not mine..
   }
 
   return v, nil
 }
+
 // Prepare HC vault secrets environment and VaultClients
-func (self *HCVaultSecretsInjectorStruct) Prep() error {
+func (self *HCVaultSecretsInjectorStruct) Prep() error { // some cleaning and cleansing.. you know orthodox stuff..
   //
   // Env Vars
   //
   secrets := make(map[string]string)
 	// prep cycle
-	for k, v := range self.EnvVars.Secrets {
+	for k, v := range self.EnvVars.Secrets { // cleanning 'em one by one. get your towels monkeys!
 		mount := strings.SplitN( v , "/" , 2 )[0]
 		// ensure kv.Client for mount
 		if _, ok := self.VaultClients[mount]; !ok {
@@ -161,22 +165,18 @@ func (self *HCVaultSecretsInjectorStruct) Prep() error {
 		}
 	}
 	self.EnvVars.Secrets = secrets
-
   return nil
 }
+
 // Pull secrets from the vault and populate self.EnvVars & self.FileVars
-func (self *HCVaultSecretsInjectorStruct) PopulateSecrets() error {
+func (self *HCVaultSecretsInjectorStruct) PopulateSecrets() error {  //  at the end of the rainbow you Shall find..
 
-  if err := self.Prep(); err != nil  {
-		return err
-	}
+  if err := self.Prep(); err != nil  {	return err  }
 
-  for k, v := range self.EnvVars.Secrets {
+  for k, v := range self.EnvVars.Secrets { // environment secrets first - age before u know..
 		// get secret from vault
 		s, err := self.VaultClients[strings.SplitN(v, "/", 2)[0]].Read(v)
-		if err != nil {
-			return err
-		}
+		if err != nil { return err }
 		if s == nil {
 			log.Warningf("Secret '%s' not found in the vault.", v)
 			continue
@@ -193,9 +193,7 @@ func (self *HCVaultSecretsInjectorStruct) PopulateSecrets() error {
     for k, v := range s.Secrets {
       log.Debugf("Attempt to read '%s' from HC vault", v)
   		s, err := self.VaultClients[strings.SplitN(v, "/", 2)[0]].Read(v)
-  		if err != nil {
-  			return err
-  		}
+  		if err != nil { return err }
   		if s == nil {
   			log.Warningf("Secret '%s' not found in the vault.", v)
   			continue
