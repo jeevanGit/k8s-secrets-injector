a91add682e548a41b0fe2f318d9beb6aca4b3cf0
oleggorj <oleggorj@hotmail.com>
2019-12-06 11:14:10 -0500

    ongoing development commits

--- src/secretsinjector/secrets-injector-common.go
+++ src/secretsinjector/secrets-injector-common.go
@@ -7,8 +7,8 @@ import (
 	"fmt"
 	"strings"
 	"errors"
-  "os"
-  _ "path"
+    "os"
+      _ "path"
 	log "github.com/sirupsen/logrus"
 
   "utils"
@@ -18,22 +18,25 @@ var vaultOrigins          = [...]string {"hashicorpvault", "AzureKeyVault"}
 
 const (
 
-	  hcVaultVarName         = "hashicorpvault"
+    hcVaultVarName         = "hashicorpvault"
   	azureVaultVarName      = "AzureKeyVault"
-  	patternSecretName      = "secret_injector_secret_name_"
-  	patternSecretMountPath = "secret_injector_mount_path_"
-  	patternStoreSystem		 = "secret_store_system_"
-    vaultPathConst         = "VAULT_PATH"
+  	patternSecretName       = "secret_injector_secret_name_"
+  	patternSecretMountPath  = "secret_injector_mount_path_"
+  	patternStoreSystem		= "secret_store_system_"
+    vaultPathConst          = "VAULT_PATH"
 )
+
 // describes the structure of any Secret (vault-agnostic)
 type SecretStruct struct {
   Name          string // actual secret's name
   Secret        string // actual secret value
-  Path          string // secret's path within the vault
+  VaultPath     string // secret's path within the vault
   Encoding      string // method of encoding secret value
   Origin        string // type of the vault
   EnvVar        string // if set secret needs to be populated as Env Var
   File          string // if set secret needs to be populated as File
+  FilePath      string // secret's path for secret file
+
 }
 
 // struct describes the chain of secrets
@@ -41,8 +44,8 @@ type SecretChainStruct struct {
   Secrets map[string]SecretStruct
 }
 
-//
-func NewSecretChain() (*SecretChainStruct, error){
+// generates brand new Chain of Secrets)
+func NewSecretChain() (*SecretChainStruct, error){    // think of it as Chamber of The Secrets..
   scs := &SecretChainStruct{}
   scs.Secrets = make(map[string]SecretStruct)
   if err := scs.init(); err != nil {  // Histon - we have a problem
@@ -50,7 +53,7 @@ func NewSecretChain() (*SecretChainStruct, error){
   }
   return scs, nil
 }
-//
+// Initialize the secrets chain
 func (self *SecretChainStruct) init() error {
 
   for _, pair := range os.Environ() { // go through env vars one by one
@@ -67,6 +70,8 @@ func (self *SecretChainStruct) init() error {
 func (self *SecretChainStruct) parse(key, val string) (*SecretStruct, error) {
   v := &SecretStruct{}
 
+  log.Debugf("----> entry %s - %s", key, val)
+
   // check if var ends with "@<something>". Use vaultOrigins as list of all possible "something"s
   if strings.Contains ( strings.ToLower(val), "@" ){  // env vars secrets..
     for _, item := range vaultOrigins {
@@ -75,12 +80,17 @@ func (self *SecretChainStruct) parse(key, val string) (*SecretStruct, error) {
         v.Name = strings.TrimSuffix( strings.ToLower(val), "@" + item )
         v.Origin = item
         v.EnvVar = key
+        // lookup VAULT_PATH to find path within the vault (it could be empty and thats cool..  totaly cool..)
+        v.VaultPath = utils.GetEnvVariableByName( vaultPathConst )
       }
     }
+    return v, nil
   }else{  // now.. file secrets..:) here come the bride..
 
-    if strings.HasPrefix( key, patternSecretName ) {             // see if var name matches SECRET_INJECTOR_SECRET_NAME_<index>
-      secIndex := strings.TrimPrefix( key, patternSecretName )   // <index>
+    if strings.HasPrefix( strings.ToLower(key), patternSecretName ) {             // see if var name matches SECRET_INJECTOR_SECRET_NAME_<index>
+      secIndex := strings.TrimPrefix( strings.ToLower(key), patternSecretName )   // <index>
+
+      log.Debugf("--> parsing %s - %s , and it matches with %s", key, val, patternSecretName)
 
       // look up corresponding Store System env var - it determines the origin vault for the secret
       v.Origin = utils.GetEnvVariableByName( patternStoreSystem + secIndex ) // see if var name matches SECRET_STORE_SYSTEM_<index>
@@ -91,26 +101,30 @@ func (self *SecretChainStruct) parse(key, val string) (*SecretStruct, error) {
 
         // look up second corresponding env var with name "secret_injector_mount_path_" + secIndex,
         //   to determine mount path for secret file
-        v.Path = strings.TrimSuffix( utils.GetEnvVariableByName( patternSecretMountPath + v.Name ) , "/") + "/"
-        if v.Path == "" {   // finding SECRET_INJECTOR_MOUNT_PATH_<index>
+        v.FilePath = strings.TrimSuffix( utils.GetEnvVariableByName( patternSecretMountPath + secIndex ) , "/") + "/"
+        if v.FilePath == "" {   // finding SECRET_INJECTOR_MOUNT_PATH_<index>
           return v, errors.New( fmt.Sprintf("Missing second set of env variables for secret %s: '%s'", v.Name, patternSecretMountPath + v.Name ) )
         }
         // Now, its Xmas time! Lets construct the name of the secret within the vault..
-        // there are 2 parts to it
-        // 1 - lookup VAULT_PATH to find path within the vault (it could be empty and thats cool..  totaly cool..)
-        v.Name = utils.GetEnvVariableByName( vaultPathConst )   // Here's a little TODO: design how distinguish vault path across diff vaults (just in case of many vaults and potentialy diff types)
-        // 2 - adding to path actual name of secret within vault
-        v.Name = v.Name + val
+        // set actual name of secret within vault
+        v.Name = val
+        v.File = val
+        // lookup VAULT_PATH to find path within the vault (it could be empty and thats cool..  totaly cool..)
+        v.VaultPath = utils.GetEnvVariableByName( vaultPathConst )   // Here's a little TODO: design how distinguish vault path across diff vaults (just in case of many vaults and potentialy diff types)
       }
 
     }
-
+    return v, nil
   }
-  return v, nil
+  return v, errors.New( fmt.Sprintf("Skipping varibale: %s", key ) )
 }
 
 // adding new secret into the chain with the key as "name-of-the-secret:origin-vault"
 //  secrets may have the same names across the vaults
 func (self *SecretChainStruct) add(s SecretStruct) {
-  self.Secrets[s.Name + ":" + s.Origin] = s
+  if s.EnvVar != "" {
+    self.Secrets[s.Name + "-" + s.Origin + "-ev"] = s
+  } else {
+    self.Secrets[s.Name + "-" + s.Origin + "-file"] = s
+  }
 }
