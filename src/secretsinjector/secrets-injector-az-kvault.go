6743e95dbde479dc6aa033dc2b6a9b04220ddcf8
oleggorj <oleggorj@hotmail.com>
2019-12-09 10:44:01 -0500

    code cleanup and minor fixes

--- src/secretsinjector/secrets-injector-az-kvault.go
+++ src/secretsinjector/secrets-injector-az-kvault.go
@@ -16,25 +16,31 @@ import (
 	"github.com/Azure/go-autorest/autorest"
 
 	"utils"
+	"secretschain"
 )
 
 // Az KeyVault struct
 type AzKeyVaultClientStruct struct {
 	Authorizer 			autorest.Authorizer
 	VaultClient			keyvault.BaseClient
-	Chain 				*SecretChainStruct
+	Chain 				*secretschain.SecretChainStruct
 }
 
 // Create new HC vault client and populate the environment in it
-func NewAzKVault() (*AzKeyVaultClientStruct, error) {
+func NewAzKVault(ch *secretschain.SecretChainStruct) (*AzKeyVaultClientStruct, error) {
 	// i do wonder what is the name of the vault that i suppose to interact with, eh?
-	nameKV := utils.GetEnvVariableByName( azureVaultVarName )
+	nameKV := utils.GetEnvVariableByName( secretschain.AzureVaultVarName)
 	if nameKV == "" { // dude! really?? check your vars..
 		return nil, errors.New( fmt.Sprintf("Can't create new instancce of Azure KVault Client - the name of KVault can not be empty.") )
 	}
 
 	var err error
 	v := &AzKeyVaultClientStruct{}
+	if ch == nil {
+		v.Chain, err = secretschain.NewSecretChain()	// let's spin up new secrets chain from the env..
+	}else{
+		v.Chain = ch
+	}
 
 	v.Authorizer, err = kvauth.NewAuthorizerFromEnvironment()  // knock knock.. who's there?
 	if err != nil {
@@ -45,27 +51,22 @@ func NewAzKVault() (*AzKeyVaultClientStruct, error) {
 	v.VaultClient = keyvault.New() // brand new! and shiny! keyVault Client! Hallelujah! Praise the Lord!
 	v.VaultClient.Authorizer = v.Authorizer
 
-	v.Chain, err = NewSecretChain()	// let's spin up teh secrets chain from the env..
 	for idx, _ := range v.Chain.Secrets { // loop through all the secrets we fished out from the env.
-		if v.Chain.Secrets[idx].Origin == azureVaultVarName { // filter out everything but Azure KeyVaul origins
+		if v.Chain.Secrets[idx].Origin == secretschain.AzureVaultVarName { // filter out everything but Azure KeyVaul origins
 			// Huston, we have Take Off!
-			log.Debugf("This baby is mine... %s\n", v.Chain.Secrets[idx])
-			// oh la-la! lets do some damage!
+			// here is where we're doing some damage and pulling secrets
 			secretResp, err := getSecret( v.VaultClient, nameKV, v.Chain.Secrets[idx].Name )  // let's take this baby out for a walk..
 			if err != nil {
 				log.Errorf("unable to generate secrets chain:  %v", err.Error()) // what the.
 			} else {
 				v.Chain.Secrets[idx].Secret = *secretResp.Value  // miracles are real!
 			}
-			log.Debugf("Next generation chooses.. what... %s\n", v.Chain.Secrets[idx])
 		}
 	}
 	return v, nil
 }
 
-//
-// Low level function to get the secret from the vault based on its name
-//
+// Low level function to get the secret from Azure KeyVault based on its name
 func getSecret(vaultClient keyvault.BaseClient, vaultname string, secname string) (result keyvault.SecretBundle, err error) {
 	log.Debugf("Making a call to:  https://%s.vault.azure.net to retrieve value for KEY: %s\n", vaultname, secname)
 	return vaultClient.GetSecret(context.Background(), "https://"+vaultname+".vault.azure.net", secname, "")
