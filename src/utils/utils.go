dfc831feecb712a023360fc3831e102dc950f0ba
oleggorj <oleggorj@hotmail.com>
2019-12-09 10:42:35 -0500

    code cleanup and minor fixes

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -2,10 +2,13 @@
 package utils
 
 import (
-
+	"github.com/Azure/go-autorest/autorest"
+	"net/http"
+	"net/http/httputil"
 	"path"
-  "strings"
+  	"strings"
 	"os"
+	log "github.com/sirupsen/logrus"
 )
 
 // FixPath inserts the API prefix for v1 style path
@@ -62,3 +65,39 @@ func GetEnvVariableByName(variableName string) string {
 	}
 	return ""
 }
+
+
+//
+// debug function
+//
+func logRequest() autorest.PrepareDecorator {
+	return func(p autorest.Preparer) autorest.Preparer {
+		return autorest.PreparerFunc(func(r *http.Request) (*http.Request, error) {
+			r, err := p.Prepare(r)
+			if err != nil {
+				log.Debugln(err)
+			}
+			dump, _ := httputil.DumpRequestOut(r, true)
+			log.Debugln(string(dump))
+			return r, err
+		})
+	}
+}
+
+//
+// debug function
+//
+func logResponse() autorest.RespondDecorator {
+	return func(p autorest.Responder) autorest.Responder {
+		return autorest.ResponderFunc(func(r *http.Response) error {
+			err := p.Respond(r)
+			if err != nil {
+				log.Debugln(err)
+			}
+			dump, _ := httputil.DumpResponse(r, true)
+			log.Debugln(string(dump))
+			return err
+		})
+	}
+}
+
